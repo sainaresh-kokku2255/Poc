b9c80ab3de9a1a88047421a547f67132d5e12cb6
sainaresh-kokku2255 <83744581+sainaresh-kokku2255@users.noreply.github.com>
2021-05-29 11:04:13 +0530

    Add files via upload

---
+++ StudentPoc/StudentPoc/src/main/java/com/ojas/service/StudentService.java
@@ -0,0 +1,27 @@
+package com.ojas.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ojas.Dao.StudentDao;
+
+import com.ojas.model.Student;
+
+@Service
+public class StudentService {
+	@Autowired
+	StudentDao stdDao;
+//	@Autowired
+//	AddressDao ado;
+	public Student createStudent(Student student) {
+		//ado.save(student.getAddress());
+		return stdDao.save(student);
+	}
+	
+	public Optional<Student> getById(Integer stdId) {
+		return stdDao.findById(stdId);
+	}
+	
+}
